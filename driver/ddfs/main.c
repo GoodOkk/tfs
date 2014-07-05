80f4632fa7d4c5db9b51e282f5c66ee5c5c69478
Andrey Smetanin <irqlevel@gmail.com>
2014-07-05 23:57:15 +0400

    reg fs / fill super

--- driver/ddfs/main.c
+++ driver/ddfs/main.c
@@ -15,7 +15,9 @@
 #include <linux/workqueue.h>
 #include <linux/timer.h>
 #include <linux/cdev.h>
-
+#include <linux/vfs.h>
+#include <linux/mount.h>
+#include <linux/buffer_head.h>
 #include <asm/uaccess.h>
 
 #include <ddfs_misc.h>
@@ -35,6 +37,90 @@ MODULE_LICENSE("GPL");
 
 
 #define SECTOR_SIZE  (PAGE_SIZE/PAGE_SECTORS)
+
+static struct ddfs_sb_info *ddfs_sb_info_create(struct super_block *sb)
+{
+	struct ddfs_sb_info *si = NULL;
+	si = kzalloc(sizeof(struct ddfs_sb_info), GFP_KERNEL);
+	if (!si) {
+		klog(KL_ERR, "kzalloc failed");
+		return NULL;
+	}
+	spin_lock_init(&si->lock);
+
+	return si;
+}
+
+static void ddfs_sb_info_free(struct ddfs_sb_info *si)
+{
+	if (si->sbh)
+		brelse(si->sbh);
+	kfree(si);	
+}
+
+static int ddfs_fill_super(struct super_block *sb, void *data, int silent)
+{
+	int err = -EINVAL;
+	struct ddfs_sb_info *si = NULL;
+	struct ddfs_super_block *dsb = NULL;
+	unsigned long offset = 0;
+
+	klog(KL_INFO, "sb=%p, data=%p, silent=%d", sb, data, silent);
+	
+	si = ddfs_sb_info_create(sb);
+	if (!si) {
+		klog(KL_ERR, "cant create sb info for sb=%p", sb);
+		return err; 
+	}
+	si->blocksize = sb_min_blocksize(sb, BLOCK_SIZE);	
+	if (!si->blocksize) {
+		klog(KL_ERR, "block_size is 0");
+		goto out_si_free;
+	}
+	sb->s_fs_info = si;
+	si->sbh = sb_bread(sb, 0);
+	if (!si->sbh) {
+		klog(KL_ERR, "unable to read superblock");
+		goto out_si_free;
+	}
+	dsb = (struct ddfs_super_block *)(((char *)si->sbh->b_data) + offset);
+	si->sb = dsb;
+	sb->s_magic = le32_to_cpu(dsb->magic);
+	if (sb->s_magic != DDFS_SUPER_MAGIC) {
+		klog(KL_ERR, "cant fount ddfs magic");
+		goto out_si_free;
+	}			
+	sb->s_maxbytes = MAX_LFS_FILESIZE;
+
+out_si_free:
+	sb->s_fs_info = NULL;
+	ddfs_sb_info_free(si);
+
+	return err;
+}
+
+static void ddfs_kill_sb(struct super_block *sb)
+{
+	klog(KL_INFO, "sb=%p", sb);
+	kill_block_super(sb);
+}
+
+static struct dentry *ddfs_mount(struct file_system_type *fs_type,
+	int flags, const char *dev_name, void *data)
+{
+	klog(KL_INFO, "fs_type=%p, flags=%x, dev_name=%s, data=%p",
+		fs_type, flags, dev_name, data);
+
+	return mount_bdev(fs_type, flags, dev_name, data, ddfs_fill_super);
+}
+
+static struct file_system_type ddfs_fs_type = {
+	.owner = THIS_MODULE,
+	.name = "ddfs",
+	.mount = ddfs_mount,
+	.kill_sb = ddfs_kill_sb,
+	.fs_flags = FS_REQUIRES_DEV,
+};
 
 static int __init ddfs_init(void)
 {	
@@ -57,10 +143,17 @@ static int __init ddfs_init(void)
 		klog(KL_ERR, "ddfs_misc_register err=%d", err);
 		goto out_srv_release;
 	}
-
+	
+	err = register_filesystem(&ddfs_fs_type);
+	if (err) {
+		klog(KL_ERR, "register_filesystem err=%d", err);
+		goto out_misc_unreg; 
+	}
 	klog(KL_INFO, "module loaded");
 	return 0;
 
+out_misc_unreg:
+	ddfs_misc_deregister();
 out_srv_release:
 	ddfs_srv_exit();		
 out_klog_release:
@@ -71,11 +164,12 @@ out:
 
 static void __exit ddfs_exit(void)
 {
-	klog(KL_INFO, "exiting");
+	klog(KL_INFO, "unregistering fs");
+	unregister_filesystem(&ddfs_fs_type);
+	klog(KL_INFO, "unregistering misc device");
 	ddfs_misc_deregister();
-	klog(KL_INFO, "going exit server");
+	klog(KL_INFO, "shutdowning server");
 	ddfs_srv_exit();
-	klog(KL_INFO, "server stopped");
 	klog(KL_INFO, "exited");
 	klog_release();
 }
--- driver/inc/ddfs.h
+++ driver/inc/ddfs.h
@@ -1,3 +1,16 @@
 #pragma once
 
+#include <linux/fs.h>
 
+struct ddfs_super_block {
+	__le32 			magic;	
+};
+
+struct ddfs_sb_info {
+	struct ddfs_super_block *sb;
+	struct buffer_head	*sbh;		
+	spinlock_t		lock;
+	int 			blocksize;	
+};
+
+#define DDFS_SUPER_MAGIC 0x31415926
